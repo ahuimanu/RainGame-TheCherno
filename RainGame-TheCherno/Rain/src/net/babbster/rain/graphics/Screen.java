54cc10086f66f80d96aa1e77f6c28cfca7a6321c
Jeffry Babb <ahuimanu@gmail.com>
2013-02-23 15:47:58 -0600

    update

---
+++ RainGame-TheCherno/Rain/src/net/babbster/rain/graphics/Screen.java
@@ -0,0 +1,16 @@
+package net.babbster.rain.graphics;
+
+public class Screen {
+	
+	private int width;
+	private int height;
+	
+	public int[] pixels;
+	
+	public Screen(int width, int height){
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+	}
+
+}
