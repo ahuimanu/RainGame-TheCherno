6219c45f5031d1d5a0355a455e59b433cca0fee1
Jeffry Babb <ahuimanu@gmail.com>
2013-02-23 14:19:49 -0600

    update

--- RainGame-TheCherno/Rain/src/net/babbster/rain/Game.java
+++ RainGame-TheCherno/Rain/src/net/babbster/rain/Game.java
@@ -5,35 +5,34 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	public static int height = 300;
-	public static int width  = height / 16 * 9;
-	public static int scale  = 3;
-	
+	public static int width = height / 16 * 9;
+	public static int scale = 3;
+
 	private Thread thread;
 	private boolean running = false;
 	private JFrame frame;
-	
-	public Game(){
+
+	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		this.setPreferredSize(size);
-		
+
 		frame = new JFrame();
 	}
-	
-	public synchronized void start(){
+
+	public synchronized void start() {
 		running = true;
 		thread = new Thread(this, "Game");
 		thread.start();
 	}
-	
-	public synchronized void stop(){
+
+	public synchronized void stop() {
 		running = false;
-		try{
+		try {
 			thread.join();
-		}
-		catch(InterruptedException error){
+		} catch (InterruptedException error) {
 			error.printStackTrace();
 		}
 	}
@@ -41,18 +40,19 @@ public class Game extends Canvas implements Runnable{
 	@Override
 	public void run() {
 		// Game loop
-		while(running){
+		while (running) {
 			System.out.println("Runing");
 		}
 	}
-    public static void main(String[] args){
-    	Game game = new Game();
-    	game.frame.setResizable(false);
-    	game.frame.setTitle("Rain");
-    	game.frame.add(game);
-    	game.frame.pack();
-    	game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	game.frame.setVisible(true);
-    	game.start();
-    }
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Rain");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setVisible(true);
+		game.start();
+	}
 }
