6a4f8d5f21b7b120bb867119c29a121b67915c18
Jeffry Babb <ahuimanu@gmail.com>
2013-02-23 11:49:31 -0600

    Upload to github

---
+++ RainGame-TheCherno/Rain/src/net/babbster/rain/Game.java
@@ -0,0 +1,58 @@
+package net.babbster.rain;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{
+	private static final long serialVersionUID = 1L;
+	public static int height = 300;
+	public static int width  = height * 16 * 9;
+	public static int scale  = 3;
+	
+	private Thread thread;
+	private boolean running = false;
+	private JFrame frame;
+	
+	public Game(){
+		Dimension size = new Dimension(width * scale, height * scale);
+		this.setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
+	
+	public synchronized void start(){
+		running = true;
+		thread = new Thread(this, "Game");
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		running = false;
+		try{
+			thread.join();
+		}
+		catch(InterruptedException error){
+			error.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		// Game loop
+		while(running){
+			System.out.println("Runing");
+		}
+	}
+    public static void main(String[] args){
+    	Game game = new Game();
+    	game.frame.setResizable(false);
+    	game.frame.setTitle("Rain");
+    	game.frame.add(game);
+    	game.frame.pack();
+    	game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	game.frame.setVisible(true);
+    	game.start();
+    }
+}
