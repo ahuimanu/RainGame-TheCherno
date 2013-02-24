aae3a7b6476735fc1538a05c4cc31b2f5634af1f
Jeffry Babb <ahuimanu@gmail.com>
2013-02-23 20:00:45 -0600

    update

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/64/00dcb2f7257e00121234e91537761191
@@ -0,0 +1,9 @@
+package net.babbster.rain.graphics;
+
+public class Sprite {
+	
+	private final int SIZE;
+	private int x, y;
+	public int[] pixels;
+
+}
--- RainGame-TheCherno/Rain/src/net/babbster/rain/graphics/Sprite.java
+++ RainGame-TheCherno/Rain/src/net/babbster/rain/graphics/Sprite.java
@@ -5,5 +5,24 @@ public class Sprite {
 	private final int SIZE;
 	private int x, y;
 	public int[] pixels;
+	private SpriteSheet sheet;
+	
+	public Sprite(int size, int x, int y, SpriteSheet sheet)
+	{
+		this.SIZE = size;
+		pixels = new int[SIZE * SIZE];
+		this.x = x * this.SIZE;
+		this.y = y * this.SIZE;
+		this.sheet = sheet;
+	}
+	
+	//loop through and copy the pixels from a zone of the sprite sheet
+	private void load(){
+		for(int y = 0; y < SIZE; y++){
+			for(int x = 0; x < SIZE; x++){
+				pixels[x + y * SIZE] = sheet.pixels[(x + this.x) + (y + this.y) * sheet.SIZE ];
+			}
+		}
+	}
 
 }
